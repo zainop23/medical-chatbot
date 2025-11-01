27cb54aaded8cdcfeae1a5decc375e01850e0618
zainop23 <anwerbasha@Zains-Mac.local>
2025-11-02 01:41:44 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Zain",
+    author_email="zainal2004abidin@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
