5a61f731388842c5f9e15e72d103491b5c5e0d69
zainop23 <anwerbasha@Zains-Mac.local>
2025-11-02 01:21:02 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating directory 
+mkdir -p src
+mkdir -p research 
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py 
+touch src/prompt.py
+touch .env 
+touch setup.py
+touch app.py
+touch research/trials.ipynb 
+touch requirements.txt
+
+
+echo "Directory and files created successfully!."
